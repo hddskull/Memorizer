21ba96e838e37ff22a873d53c306d9b4dd8e7c8a
Max <maxdgladkov@gmail.com>
2021-12-08 12:08:49 +0300

    Initial Commit

---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Max Gladkov on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
